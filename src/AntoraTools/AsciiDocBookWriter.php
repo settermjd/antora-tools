a8601bdc1288914e5e3828835189e75a57f4cd32
Matthew Setter <matthew@matthewsetter.com>
2018-08-24 13:17:04 +0200

    Add the AsciiDocBookWriter class
    This class is responsible, via a template rendering layer, for writing
    the contents of an AsciiDoc book.adoc file, which is the configuration
    file that asciidoctor-pdf uses to generate a PDF file from a collection
    of AsciiDoc files.

---
+++ src/AntoraTools/AsciiDocBookWriter.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace AntoraTools;
+
+use Zend\Expressive\Template\TemplateRendererInterface;
+
+class AsciiDocBookWriter
+{
+	const BOOK_TEMPLATE = 'book.adoc.twig';
+
+	/**
+	 * @var $templateRenderer TemplateRendererInterface
+	 */
+	private $templateRenderer;
+
+	/**
+	 * @var array core book.adoc file options
+	 */
+	private $options;
+
+	/**
+	 * AsciiDocBookWriter constructor.
+	 *
+	 * @param TemplateRendererInterface $templateRenderer
+	 * @param array $options
+	 */
+	public function __construct(TemplateRendererInterface $templateRenderer, array $options)
+	{
+		$this->templateRenderer = $templateRenderer;
+		$this->options = $options;
+	}
+
+	/**
+	 * Generate the book's contents
+	 *
+	 * @param array $filesList The list of files to include in the book
+	 * @return string
+	 */
+	public function generateBookContents(array $filesList) : string
+	{
+		return $this->templateRenderer->render(self::BOOK_TEMPLATE, [
+			'title' => $this->options['title'],
+			'homepage' => $this->options['homePage'],
+			'imagesdir' => $this->options['imagesDir'],
+			'version' => $this->options['version'],
+			'buildDate' => $this->options['buildDate'],
+			'fileBasePath' => $this->options['fileBasePath'],
+			'files' => $filesList,
+		]);
+	}
+}
