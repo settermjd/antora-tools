1a19415b40a353049413699403c4132b7cf5a435
Matthew Setter <matthew@matthewsetter.com>
2018-08-27 15:12:29 +0200

    Fixed issue with configuring the templates directory
    While it worked just fine in testing, in the prod code there was a
    mistake in the path to the templates directory. Not sure how to best
    test this, yet.

--- application.php
+++ application.php
@@ -3,12 +3,13 @@
 require __DIR__.'/vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
+use Zend\Expressive\Twig\TwigRenderer;
 
 $application = new Application();
 
 $renderer = new TwigRenderer(
-	new Twig_Environment(
-		new Twig_Loader_Filesystem(__DIR__ . '/../templates'),
+	new \Twig_Environment(
+		new \Twig_Loader_Filesystem(__DIR__ . '/src/AntoraTools/templates'),
 		[]
 	)
 );
